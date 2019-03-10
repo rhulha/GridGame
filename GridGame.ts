e8bfd61d31fda86c389e65e9f526aa678e74857e
Raymond Hulha <java4life@gmail.com>
2019-03-10 02:49:52 +0100

    Update GridGame.ts
    added getRandomInt(min: number, max: number): number

--- GridGame.ts
+++ GridGame.ts
@@ -112,5 +112,8 @@ export class GridGame<T> {
       }
     });
   }
+}
 
+export function getRandomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
